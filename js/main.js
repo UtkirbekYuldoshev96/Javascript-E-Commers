5d6cc4a08bce79930f11121e3298ed065bd8f081
utkirbek Yuldoshev <utkirbekyuldoshev7@gmail.com>
2022-04-04 12:24:14 +0500

    E-commer click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 window.addEventListener('click', (event) => {
-	console.log('Click window');
 
-	console.log(event);
+	const counterWrapper = event.target.closest('.counter-wrapper');
+	const counter = counterWrapper.querySelector('[data-counter]');
+	
+		if(event.target.dataset.action === "plus"){
+			counter.innerText = ++counter.innerText;
+		}
 
+		if(event.target.dataset.action === "minus"){
+
+			const counterWrapper = event.target.closest('.counter-wrapper');
+			const counter = counterWrapper.querySelector('[data-counter]');
+
+			if (parseInt(counter.innerText) > 1 ) {
+			counter.innerText = --counter.innerText;	
+			}
+	}
 });
